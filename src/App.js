768ee3625ceff0c1f311c7b06dbb20815d65c19c
tabby <tlhorn88@gmail.com>
2023-02-21 09:09:06 -0600

    second update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        console.log('Need to add book with:', title);
+        const updatedBooks = [
+            ...books,
+            // { id: 123, title: title } The following line is the same
+            // { id: 123, title }
+            {
+                id: Math.round(Math.random() * 9999),
+                title,
+            },
+        ];
+        setBooks(updatedBooks);
     };
 
     return <div>
+        {books.length}
         <BookCreate onCreate={createBook} />
     </div>;
 }
